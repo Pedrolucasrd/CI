35038996170b720b1af9876c0beccd8ae88e9f21
Pedro Lucas Rodrigues <101150701+Pedrolucasrd@users.noreply.github.com>
2023-09-21 16:04:48 -0300

    pom teste

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/teste-cd")
+    public String testeCd(){
+        return "Teste bem sucedido";
+    }
+}
